ba58a876066fff5733f783fa936bc877035710f4
Oliver Jumpertz <oliver.jumpertz@gmail.com>
2022-03-11 23:12:27 +0100

    feat: add activity view thesis

---
+++ app/src/main/java/com/example/supervisionapp/ActivityViewThesisRequest.java
@@ -0,0 +1,62 @@
+package com.example.supervisionapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.supervisionapp.data.LoginRepository;
+import com.example.supervisionapp.databinding.ActivitySubmitThesisBinding;
+import com.example.supervisionapp.ui.login.LoginActivity;
+
+public class ActivityViewThesisRequest extends AppCompatActivity {
+
+    private ActivitySubmitThesisBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySubmitThesisBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // TODO: retrieve bundle and use to fill ViewModel, etc.
+
+        View arrowBack = findViewById(R.id.activity_submit_thesis_backArrow);
+        arrowBack.setClickable(true);
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        View buttonSend = findViewById(R.id.activity_submit_thesis_buttonSend);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO submit...
+                finish();
+            }
+        });
+
+        View logout = findViewById(R.id.activity_submit_thesis_appBar_logout);
+        logout.setClickable(true);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickLogout(view);
+            }
+        });
+    }
+
+    public void onClickLogout(View view) {
+        LoginRepository loginRepository = LoginRepository.getInstance(null);
+        loginRepository.logout();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        // prevent users from going back in history after they logged out
+        finish();
+    }
+}
