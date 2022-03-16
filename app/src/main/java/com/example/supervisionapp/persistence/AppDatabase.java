fd905398953f70ee66db66b7fb673c1be276b241
Oliver Jumpertz <oliver.jumpertz@gmail.com>
2022-03-16 21:36:34 +0100

    feat: implement app persistence provider

--- app/src/main/java/com/example/supervisionapp/persistence/AppDatabase.java
+++ app/src/main/java/com/example/supervisionapp/persistence/AppDatabase.java
@@ -1,6 +1,9 @@
 package com.example.supervisionapp.persistence;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {
@@ -15,13 +18,39 @@ import androidx.room.RoomDatabase;
         UserType.class
 }, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase INSTANCE;
+
+    static AppDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room
+                            .databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class,
+                                    "app_database")
+                            // TODO: add initial data
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
     public abstract InvoiceStateDao invoiceStateDao();
+
     public abstract StudentDao studentDao();
+
     public abstract SupervisorDao supervisorDao();
+
     public abstract SupervisoryStateDao supervisoryStateDao();
+
     public abstract SupervisoryTypeDao supervisoryTypeDao();
+
     public abstract ThesisDao thesisDao();
+
     public abstract ThesisStateDao thesisStateDao();
+
     public abstract UserDao userDao();
+
     public abstract UserTypeDao userTypeDao();
 }
